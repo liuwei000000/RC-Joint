bdde114386f7471cf1bba79ecddcdecca2ceeb8e
Liu <Wei>
2018-11-02 00:25:36 +0800

    更新架构

--- st.js
+++ st.js
@@ -3,7 +3,7 @@ const Stick = require('stickpackage').stick;
 //const stick = new Stick(1024).setReadIntBE('16')
 const HEAD_LEN = 2
 
-var sockets = {};
+
 
 function data_process(data) {
     const head = new Buffer(HEAD_LEN)
@@ -22,24 +22,59 @@ function data_process(data) {
     data.copy(body, 0, HEAD_LEN, HEAD_LEN + l)
     console.log('DD:', body)
 }
+var clientList = {};
+/**
+ * 释放连接资源
+ */
+function freeConn(conn) {
+    delete clientList[conn.uname];
+    conn.timer && clearInterval(conn.timer);
+    conn.destroy();
+}
 
-const tcp_server = net.createServer(function (socket) {
+function addCoon(conn, name) {
+    conn.uname = name;
+    clientList[name] = conn;
+    conn.timer = setInterval(() => {
+        // 发送心跳信息
+        // 测试 freeConn(conn);
+    }, 3000);
+}
+
+function sendMsg(conn, data) {
+    if (!conn.writable)  freeConn(conn); //防止超时，没有收到end包
+    conn.write(data);
+}
+
+const tcp_server = net.createServer({ allowHalfOpen: false }, function (socket) {
     //连接成功进入
     //socket.write("hello,i'm server!");
-    console.log('客户端：connnet');
+    console.log('client connnet', Object.keys(clientList).length);
+    addCoon(socket, "");
 
     socket.on('data', function (data) {
-        console.log("Dat", data)
+        console.log("Dat", data, data.length);
+        console.log("================");
     })
 
     socket.on('close', function (error) {
-        console.log('client disconnected')
+        console.log('client close');
+        freeConn(socket);
     })
 
     socket.on('error', function (error) {
         console.log(`error:客户端异常断开: ${error}`)
     })
+
+    socket.on('end', function (error) {
+        console.log('client end');
+        socket.destroy();
+    })
+
+    
 })
+
+
 
 tcp_server.on('error', function (err) {
     throw err
